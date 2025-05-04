c8e21f281f476ba59cf77d76d6ade2581822c077
Mostafa <mostafa@Mostafa-CWAs-Laptop.local>
2025-05-04 20:31:32 +0600

    add: timestamp dynamically on html

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # This script generates a simple HTML report for the weather.
 # It creates an HTML file with a title and a paragraph displaying the temperature.
+from datetime import datetime
+
 def generate_report():
     html = """
     <html>
@@ -14,6 +16,18 @@ def generate_report():
     with open("report.html", "w") as f:
         f.write(html)
     return True
+
+# Current timestamp
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# HTML file read and add timestamp
+with open("report.html", "r") as file:
+    html_content = file.read()
+
+html_content = html_content.replace("{{timestamp}}", timestamp)
+
+with open("report.html", "w") as file:
+    file.write(html_content)
 
 if __name__ == "__main__":
     generate_report()
